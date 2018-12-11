2fb37c4bced71746fa57c0e653f5e8ae645d3ff4
vlad_mag_ <vladmagcool@gmail.com>
2018-12-11 18:15:55 +0300

    vlad0mag0

--- public/add.js
+++ public/add.js
@@ -1,1 +1,7 @@
-alert("hello")
+window.onload = function(){
+	var k=0
+	for(k=1;k<4;k++){
+  		var turn = document.getElementById("Player2").children[k]
+  		turn.disabled=true;
+  }
+}
--- src/App.js
+++ src/App.js
@@ -8,10 +8,33 @@ constructor(props){
 
   this.state={
     currentplayer: 1,
+    winner: 0,
     string1: "Your turn!",
     string2: "",
   }
+  this.checking = this.checking.bind(this);
 }
+
+checking(){
+ var p=0
+ var num=0
+ for(p=0;p<4;p++){
+  num+=document.getElementById("level-"+p+"").children.length
+ }
+ if (num===0){
+    var background=document.getElementById('end')
+    background.style.display="block"
+    this.setState({
+      winner: this.state.currentplayer
+    })  
+    return(false)
+ }
+ else{
+  return(true)
+ }
+}
+
+
 
 removeMatches(amount){
   var i = 0;
@@ -25,26 +48,48 @@ removeMatches(amount){
           var level= document.getElementById("level-"+radion+"")
           level.removeChild(level.firstChild)
           ;}
-          }
-      }
-      }
-  this.setState({
+      var next=this.checking()
+      if (next===true){
+        this.setState({
     currentplayer: 3 - this.state.currentplayer
     },() => { 
-      alert(this.state.currentplayer)
-      if(this.state.currentplayer=1){
+      if(this.state.currentplayer===1){
+        var k=0
+        for(k=1;k<4;k++){
+          var turn = document.getElementById("Player2").children[k]
+          turn.disabled=true;
+        }
+        for(k=1;k<4;k++){
+          var turn = document.getElementById("Player1").children[k]
+          turn.disabled=false;
+        }
         this.setState({
           string1:"Your Turn!!!",
           string2:""
             })
             }
-      if(this.state.currentplayer=2){
+      if(this.state.currentplayer===2){
+        for(k=1;k<4;k++){
+          var turn = document.getElementById("Player2").children[k]
+          turn.disabled=false;
+        }
+        for(k=1;k<4;k++){
+          var turn = document.getElementById("Player1").children[k]
+          turn.disabled=true;
+        }
         this.setState({
           string1:"",
           string2:"Your Turn!!!"
             })
           }
         } )
+      }    
+          }
+      else{
+        alert("You couldn't take so much matches from this yard!!!")
+      }
+      }
+      }
     }
 
 
@@ -55,6 +100,10 @@ removeMatches(amount){
   render() {
     return (
       <div id="page">
+        <div id ="end">
+          <span id="backg">The game is over!!!</span>
+          <span id="winner">The player with number {this.state.winner} is winner</span>
+        </div>
         <div id="main">
           <div id="Player1">
           <span id="titlePlayer1">Player 1</span>
